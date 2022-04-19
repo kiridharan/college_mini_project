6af6f46d5d012a0dbe1a6a7439530f70cc8e8ba8
kiridharan <kiridharan217dec2001@gmail.com>
2022-04-19 22:17:01 +0530

    full stack

---
+++ roboflow-customer-demo/robo.py
@@ -0,0 +1,22 @@
+import io
+import cv2
+import requests
+from PIL import Image
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+# Load Image with PIL
+img = cv2.imread("2.jpg")
+image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+pilImage = Image.fromarray(image)
+
+# Convert to JPEG Buffer
+buffered = io.BytesIO()
+pilImage.save(buffered, quality=100, format="JPEG")
+
+# Build multipart form and post request
+m = MultipartEncoder(fields={'file': ("imageToUpload", buffered.getvalue(), "image/jpeg")})
+
+response = requests.post("https://detect.roboflow.com/plastic-detection-rypmj/1?api_key=ydcfZusbpX9onf7r3VsZ", data=m, headers={'Content-Type': m.content_type})
+
+print(response)
+print(response.json())
